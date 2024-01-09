599cb6b59b399aa9194900f7c0f732d863f3a4f0
Daniel Kingston <dnlkingston75@gmail.com>
2024-01-10 11:48:46 +1300

    landing page structure

--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -1,3 +1,25 @@
 export default function Landing() {
-  return <h1>Bruh</h1>
+  return (
+    <div className="landing">
+      <div className="landing-top">
+        <div className="landing__header">
+          <h1 className="landing-heading"></h1>
+          <div className="landing__img"></div>
+        </div>
+      </div>
+      <div className="landing-bottom">
+        <div className="landing-bottom__left">
+          <ul className="landing__nav">
+            <li className="landing-nav-item">DEV | DESIGN WORK</li>
+            <li className="landing-nav-item">PHOTOGRAPHY</li>
+            <li className="landing-nav-item">GET IN TOUCH</li>
+            <li className="landing-nav-item">ABOUT</li>
+          </ul>
+        </div>
+        <div className="landing-bottom__right">
+          <h2 className="landing-heading">Daniel Kingston</h2>
+        </div>
+      </div>
+    </div>
+  )
 }

fd472d4fc1eae06b3619a6ab69d4d8d07be6079f
Daniel Kingston <dnlkingston75@gmail.com>
2024-01-30 12:01:32 +1300

    backrgound added home-page

--- server/routes/work.ts
+++ server/routes/work.ts
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   try {
     const work = await db.getAllWork()
 
-    res.json({ work: work.map((work) => work.title) })
+    res.json({ work: work.map((work) => work) })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })

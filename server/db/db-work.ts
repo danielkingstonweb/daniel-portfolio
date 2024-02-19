9af473eec0c460e199535f0ed6ea2020b8a95c93
Daniel Kingston <dnlkingston75@gmail.com>
2024-02-19 21:30:39 +1300

    Single work DB and Routes

--- server/db/db-work.ts
+++ server/db/db-work.ts
@@ -2,5 +2,12 @@ import connection from './connection.ts'
 import { DesWork } from '../../models/deswork.ts'
 
 export async function getAllWork(db = connection): Promise<DesWork[]> {
-  return db('work').select()
+  return await db('work').select()
+}
+
+export async function getSingleWork(
+  id: number,
+  db = connection,
+): Promise<DesWork[]> {
+  return await db('work').select().where('id', id)
 }
--- server/routes/work-routes.ts
+++ server/routes/work-routes.ts
@@ -7,11 +7,19 @@ const router = Router()
 router.get('/', async (req, res) => {
   try {
     const work = await db.getAllWork()
-
     res.json(work)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/Work/:workId', async (req, res) => {
+  try {
+    const workId = Number(req.params.workId)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'uh oh, no Work!' })
   }
 })
 
